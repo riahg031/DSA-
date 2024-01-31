ed641c426558fe8cac5fc682b9814b25c22a940b
Ria <riasharmaghy@gmail.com>
2024-01-31 22:19:47 +0530

    Longest subarray length with sum k

--- src/Arrays/LongestSumSubarray.java
+++ src/Arrays/LongestSumSubarray.java
@@ -4,12 +4,18 @@ import java.util.HashMap;
 
 public class LongestSumSubarray {
     public static void main(String[] args){
-        int[] arr = {1,2,3,1,1,1,1,3,3};
+        int[] arr = {1,2,3,1,1,1,0, 0, 1,3,3};
+//        int[] arr  = {2, 0, -1, 0, 0, 3};
         int sum = 3;
+        //Brute force(For all)
         System.out.println(longestSubarraySum(arr, sum));
+        //Better approach(For all)
         System.out.println(longestSubarraySum2(arr, sum));
+        //Optimal approach (for +ves and 0)
+        System.out.println(longestSubarraySum3(arr, sum));
     }
     // O(n^2) : Using two loops
+    //SC: O(1)
     public  static int longestSubarraySum(int[] arr, int k){
         int length=0;
         for(int i=0; i<arr.length; i++){
@@ -23,48 +29,54 @@ public class LongestSumSubarray {
         return length;
     }
 
-    // O(n* logn) : Using hashing
+    // O(n* logn) : Using hashing (+ve, -ve, 0)
+    // SC: O(n);
     public static int longestSubarraySum2(int[] arr,int k){
         //Prefix sum map
         HashMap<Integer, Integer> prefixMap = new HashMap<>();
         int length=0;
+        int sum = 0;
 
         for(int i=0; i<arr.length; i++) {
-            int sum = 0;
             sum += arr[i];
-//
-//            //Sum equals to k
-//            if(sum == k)
-//                length = Math.max(length, i+1);
-//
-//            //Add element to hashmap
-//            if(!prefixMap.containsKey(sum))
-//                prefixMap.put(sum, i);
-//
-//            //Check if remainder(sum-k) is present is hashmap => subarray with sum=k like between ( prefixMap.getValue(sum-k) - i +1)
-//            int rem = sum-k;
-//            if(prefixMap.containsKey(rem)){
-//                int len = i - prefixMap.get(rem);
-//                length = Math.max(length, len);
-//            }
 
-            // if the sum = k, update the maxLen:
-            if (sum == k)
-                length = Math.max(length, i + 1);
+            //Sum equals to k
+            if(sum == k)
+                length = Math.max(length, i+1);
 
-
-            // calculate the sum of remaining part i.e. x-k:
-            long rem = sum - k;
-
-            //Calculate the length and update maxLen:
-            if (prefixMap.containsKey(rem)) {
+            //Check if remainder(sum-k) is present is hashmap => subarray with sum=k like between ( prefixMap.getValue(sum-k) - i +1)
+            //Check if subarray exists in between array elements
+            int rem = sum-k;
+            if(prefixMap.containsKey(rem)){
                 int len = i - prefixMap.get(rem);
                 length = Math.max(length, len);
             }
 
-            //Finally, update the map checking the conditions:
-            if (!prefixMap.containsKey(sum))
+            //Add sum to hashmap if it doesn't exists
+            if(!prefixMap.containsKey(sum))
                 prefixMap.put(sum, i);
+        }
+        return length;
+    }
+
+    //O(2N): Two opinters approach
+    //SC: O(1)
+    public static int longestSubarraySum3(int[] arr, int k){
+        int length=0, sum=arr[0];
+        int left=0, right=0;
+
+        while(right < arr.length){
+            while(left <= right && sum > k){
+                sum -= arr[left];
+                left++;
+            }
+
+            if(sum == k)
+                length = Math.max(length, right-left+1);
+
+            right++;
+            if(right < arr.length)
+                sum += arr[right];
         }
         return length;
     }
